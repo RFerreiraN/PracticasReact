df89a37f16bbc2511122661af558815dc6485d96
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-04 23:58:43 +0200

    practica

--- src/Components/FetchUsers.jsx
+++ src/Components/FetchUsers.jsx
@@ -1,27 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const FetchUsers = () => {
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [endPoint, setEndPoint] = useState('users')
 
-  const FetchUser = async() => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const datos = await response.json()
-    setUsers(datos)
-    console.log(datos)
-    console.log('Has llamado a la API')
+  const fetchUser = async() => {
+    try {
+       const response = await fetch(`https://jsonplaceholder.typicode.com/${endPoint}`);
+       const datos = await response.json()
+       setUsers(datos)
+       console.log(datos)
+       console.log('Has llamado a la API')
+    } catch (error) {
+      console.error( error )
+    }
+   
   }
 
   const llamarAPI = () => {
-    FetchUser()
+    fetchUser()
   }
+
+  const llamarComentarios = () => {
+    setEndPoint('comments')
+  }
+
 
   return (
     <>
       <ul>
-         { users.map( user => <li key={user.id}> { user.name }</li>)}
+         { users.map( user => <li key={user.id}>  { user.name } </li>)}
       </ul>
-      <button onClick={llamarAPI}>Llamar</button>
+      <p>
+          <button onClick={llamarAPI}>Llamar Usuarios</button>
+      </p>
+
+      <p>
+          <button onClick={llamarComentarios}> Traer Comentarios</button>
+      </p>
+      
     </>
   )
 }
+
+
+//
