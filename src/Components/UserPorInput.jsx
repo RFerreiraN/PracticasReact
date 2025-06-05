3e609ee349bde64349078083384dde3ee3809590
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-05 13:43:09 +0200

    practica finalizada

--- src/Components/UserPorInput.jsx
+++ src/Components/UserPorInput.jsx
@@ -1,44 +1,53 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const UserPorInput = () => {
 
-  const [userID, setUserID] = useState('');
-  
+  const [userID, setUserID] = useState(''); // lo usanmos para definir el id del usaurio 
+  const [datosUsuario, setDatosUsuario] = useState() // lo usamos para guardar los datos del usuario
 
-  const userValue = ( event ) => {
-    setUserID(event.target.value)
-    console.log(userID)
-  }
-
-  const fetchInput = async() => {
+  const fetchInput = async () => {
     try {
       const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userID}`);
       const data = await response.json()
+      setDatosUsuario(data)
       console.log(data)
     } catch (error) {
-      console.error( error )
+      console.error(error)
     }
   }
 
-  const onSubmitInput = (event) => {
-      event.preventDefault()
-      
-      setUserID('')
+  const userValue = (event) => {
+      setUserID(event.target.value)
   }
 
-  useEffect(() => { 
+  const onSubmitInput = (event) => {
+    event.preventDefault()
     fetchInput()
-  }, [])
+    setUserID('')
+  }
+
 
   return (
-    <form onSubmit={onSubmitInput}>
-      <h1>Busqueda por ID:</h1>
+    <>
+      <form onSubmit={onSubmitInput}>
+        <h1>Busqueda por ID:</h1>
 
-      <input type="text"
-             placeholder="Ingrese ID"
-             value={userID}  
-             onChange={userValue}      
-      />
-    </form>
+        <input type="text"
+          placeholder="Ingrese ID"
+          value={userID}
+          onChange={userValue}
+        />
+      </form>
+
+      <ul>
+        { datosUsuario ? <li> { datosUsuario.name } </li>
+                       : <li>Datos no encontrados</li>                
+        }
+      </ul>
+    </>
+
+
   )
 }
+
+
